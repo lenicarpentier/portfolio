4c8675d89849db69e3c1280eac3f42321df986d2
Léni Carpentier <leni.carpentier@proton.me>
2026-06-05 18:24:04 +0200

    Add real content+small fixes

--- assets/js/translations.js
+++ assets/js/translations.js
@@ -1,8 +1,8 @@
 const translations = {
   fr: {
     nav: {
-      vectoriel: "Vectoriel",
       aquarelle: "Aquarelle",
+      vectoriel: "Vectoriel",
       boutique: "Boutiques",
       me: "À Propos",
       contact: "Contact",
@@ -16,63 +16,36 @@ const translations = {
     },
 
     sections: {
-      vectoriel: "Design vectoriel",
       aquarelle: "Aquarelle",
+      vectoriel: "Design vectoriel",
       boutique: "Boutiques",
       me: "À Propos De Moi",
       newsletter: "Newsletter",
       contact: "Contact",
     },
 
-    vectorCards: [
-      {
-        title: "Affiche expérimentale",
-        text: "Composition géométrique et tension graphique.",
-      },
-      {
-        title: "Série éditoriale",
-        text: "Exploration typographique minimaliste.",
-      },
-      {
-        title: "Identité visuelle",
-        text: "Système vectoriel et langage de formes.",
-      },
+    aquaCards: [
+      { title: "Couleurs et nature", text: "Association naturelle et chaudement colorée." },
+      { title: "Paysage monochromatique", text: "Couleurs diffuses et équilibre sensible." },
+      { title: "Reproduction animale", text: "Superposition et dilution pigmentaire."},
     ],
 
-    aquaCards: [
-      { title: "Carnet d’étude", text: "Textures organiques et transparence." },
-      {
-        title: "Paysage abstrait",
-        text: "Couleurs diffuses et équilibre sensible.",
-      },
-      {
-        title: "Recherche chromatique",
-        text: "Superposition et dilution pigmentaire.",
-      },
+    vectorCards: [
+      { title: "Création d'icônes", text: "Composition géométrique et tension graphique.", text2: "OMTake application" },
+      { title: "Mise en page", text: "Exploration typographique minimaliste.", text2: "OMTake application" },
+      { title: "Identité visuelle", text: "Système vectoriel et langage de formes." },
     ],
 
     boutiqueCards: [
       { title: "Redbubble", text: "Boutique de Léni Carpentier | Redbubble" },
       { title: "Dashery", text: "Léni | Boutique Officielle" },
-      {
-        title: "Boutiques d'impression à la demande",
-        text: "Découvrez ici une sélection de produits imprimés à la demande avec mes créations.<br><br>Vous pouvez, au travers de ces articles, me soutenir dans ma pratique artistique.<br><br>Merci énormément!",
-      },
+      { title: "Boutiques d'impression à la demande", text: "Découvrez ici une sélection de produits imprimés à la demande avec mes créations.<br><br>Vous pouvez, au travers de ces articles, me soutenir dans ma pratique artistique.<br><br>Merci énormément!" },
     ],
 
     meCards: [
-      {
-        title: "Personnalité",
-        text: "Je m’appelle Léni Carpentier.<br><br>Je suis un jeune homme observateur, sensible et passionné par la création artistique. J’aime explorer différentes techniques et styles pour reproduire le monde depuis mon point de vue, à travers l’art.<br><br>Je reste ouvert aux critiques constructives, ainsi qu'aux projets souhaités par les amateurs de mon art.",
-      },
-      {
-        title: "Activité",
-        text: "Je suis passionné par la création vectorielle et aquarelle, comme vous l'avez déjà constaté. J’aime concevoir des expériences claires, modernes et intuitives, où chaque détail a un rôle dans la vision qu'apporte chacun sur mes œuvres.<br><br>Actuellement en formation, je développe mes compétences à travers des projets personnels et m'enrichis dans la pratique.<br><br>Mon objectif est de continuer à progresser afin de pouvoir un jour devenir professionnel dans ce domaine.<br><br>Et c'est en partie grâce à votre soutien que je peux continuer à avancer dans cette voie, je vous en remercie chaleureusement!",
-      },
-      {
-        title: "Style",
-        text: "J'aime ce qui est sobre, agréablement coloré et propre. Je cherche à créer des œuvres qui dégagent une atmosphère apaisante et équilibrée, tout en étant visuellement attrayantes.<br><br>Mon style se caractérise par une utilisation harmonieuse des couleurs, des formes épurées et une attention particulière aux détails, afin de transmettre une émotion ou une idée de manière claire et esthétique.",
-      },
+      { title: "Personnalité", text: "Je m’appelle Léni Carpentier.<br><br>Je suis un jeune homme observateur, sensible et passionné par la création artistique. J’aime explorer différentes techniques et styles pour reproduire le monde depuis mon point de vue, à travers l’art.<br><br>Je reste ouvert aux critiques constructives, ainsi qu'aux projets souhaités par les amateurs de mon art." },
+      { title: "Activité", text: "Je suis passionné par la création vectorielle et aquarelle, comme vous l'avez déjà constaté. J’aime concevoir des expériences claires, modernes et intuitives, où chaque détail a un rôle dans la vision qu'apporte chacun sur mes œuvres.<br><br>Actuellement en formation, je développe mes compétences à travers des projets personnels et m'enrichis dans la pratique.<br><br>Mon objectif est de continuer à progresser afin de pouvoir un jour devenir professionnel dans ce domaine.<br><br>Et c'est en partie grâce à votre soutien que je peux continuer à avancer dans cette voie, je vous en remercie chaleureusement!" },
+      { title: "Style", text: "J'aime ce qui est sobre, agréablement coloré et propre. Je cherche à créer des œuvres qui dégagent une atmosphère apaisante et équilibrée, tout en étant visuellement attrayantes.<br><br>Mon style se caractérise par une utilisation harmonieuse des couleurs, des formes épurées et une attention particulière aux détails, afin de transmettre une émotion ou une idée de manière claire et esthétique."},
     ],
 
     newsletter: {
@@ -110,8 +83,8 @@ const translations = {
 
   en: {
     nav: {
-      vectoriel: "Vector",
       aquarelle: "Watercolor",
+      vectoriel: "Vector",
       boutique: "Shops",
       me: "About",
       contact: "Contact",
@@ -125,51 +98,36 @@ const translations = {
     },
 
     sections: {
-      vectoriel: "Vector Design",
       aquarelle: "Watercolor",
+      vectoriel: "Vector Design",
       boutique: "Shops",
       me: "About Me",
       newsletter: "Newsletter",
       contact: "Contact",
     },
 
-    vectorCards: [
-      {
-        title: "Experimental Poster",
-        text: "Geometric composition and graphic tension.",
-      },
-      { title: "Editorial Series", text: "Minimalist typography." },
-      { title: "Visual Identity", text: "Vector system and shapes." },
-    ],
-
     aquaCards: [
-      { title: "Study Sketchbook", text: "Organic textures and transparency." },
+      { title: "Colors and nature", text: "Natural color associations and warm tones." },
       { title: "Abstract Landscape", text: "Soft color balance." },
       { title: "Chromatic Research", text: "Layering and dilution." },
+    ],
+
+    vectorCards: [
+      { title: "Experimental Poster", text: "Geometric composition and graphic tension.", text2: "OMTake application" },
+      { title: "Editorial Series", text: "Minimalist typography.", text2: "OMTake application" },
+      { title: "Visual Identity", text: "Vector system and shapes." },
     ],
 
     boutiqueCards: [
       { title: "Redbubble", text: "Léni Carpentier Shop | Redbubble" },
       { title: "Dashery", text: "Léni | Official Merch Store" },
-      {
-        title: "Print on demand stores",
-        text: "Discover here a selection of products printed on demand with my creations.<br><br>Through these articles, you can support me in my artistic practice.<br><br>Thank you so much!",
-      },
+      { title: "Print on demand stores", text: "Discover here a selection of products printed on demand with my creations.<br><br>Through these articles, you can support me in my artistic practice.<br><br>Thank you so much!",},
     ],
 
     meCards: [
-      {
-        title: "Personality",
-        text: "My name is Léni Carpentier.<br><br>I am an observant young man, sensitive and passionate about artistic creation. I like to explore different techniques and styles to reproduce the world from my point of view, through art.<br><br>I remain open to constructive criticism, as well as to the projects desired by lovers of my art.",
-      },
-      {
-        title: "Activities",
-        text: "I am passionate about vector and watercolor creation, as you have already noticed. I like to design clear, modern and intuitive experiences, where each detail has a role in the vision that everyone brings to my works.<br><br>Currently in training, I am developing my skills through personal projects and enriching myself in practice.<br><br>My goal is to continue progressing so that I can one day become a professional in this field.<br><br>And it is partly thanks to your support that I can continue to move forward on this path, for which I warmly thank you!",
-      },
-      {
-        title: "Style",
-        text: "I like what is sober, pleasantly colorful and clean. I seek to create works that exude a calming and balanced atmosphere, while being visually appealing.<br><br>My style is characterized by a harmonious use of colors, clean shapes and a particular attention to detail, in order to convey an emotion or an idea clearly and aesthetically.",
-      },
+      { title: "Personality", text: "My name is Léni Carpentier.<br><br>I am an observant young man, sensitive and passionate about artistic creation. I like to explore different techniques and styles to reproduce the world from my point of view, through art.<br><br>I remain open to constructive criticism, as well as to the projects desired by lovers of my art." },
+      { title: "Activities", text: "I am passionate about vector and watercolor creation, as you have already noticed. I like to design clear, modern and intuitive experiences, where each detail has a role in the vision that everyone brings to my works.<br><br>Currently in training, I am developing my skills through personal projects and enriching myself in practice.<br><br>My goal is to continue progressing so that I can one day become a professional in this field.<br><br>And it is partly thanks to your support that I can continue to move forward on this path, for which I warmly thank you!"},
+      { title: "Style", text: "I like what is sober, pleasantly colorful and clean. I seek to create works that exude a calming and balanced atmosphere, while being visually appealing.<br><br>My style is characterized by a harmonious use of colors, clean shapes and a particular attention to detail, in order to convey an emotion or an idea clearly and aesthetically."},
     ],
 
     newsletter: {
